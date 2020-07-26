19660093baf60c169a39a3315a5743348a6ca93b
Kim <rnlcksgrp1@naver.com>
2020-07-26 16:29:56 +0900

    MOD recycle_matziplist

--- app/src/main/java/com/example/matzip_exe/adapter/MatZipListAdapter.kt
+++ app/src/main/java/com/example/matzip_exe/adapter/MatZipListAdapter.kt
@@ -17,26 +17,19 @@ import com.example.matzip_exe.receivers.NetworkReceiver
 
 class MatZipListAdapter(private val itemList: ArrayList<ModelMatZipList>,
                         private var area: String, private var region: String
-):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val TYPE_VISIT = 0
-    private val TYPE_DISTANCE = 1
-    private val TYPE_AVGCOST = 2
-
-    inner class VisitViewHolder(private val v:View):RecyclerView.ViewHolder(v), NetworkConnectedListener{
+):RecyclerView.Adapter<MatZipListAdapter.ViewHolder>() {
+    inner class ViewHolder(private val v:View):RecyclerView.ViewHolder(v), NetworkConnectedListener{
         private val networkReceiver = NetworkReceiver()
 
-        val text_matziplist_visit_seq = v.findViewById<TextView>(R.id.text_matziplist_visit_seq)
-        val img_matziplist_visit_type = v.findViewById<ImageView>(R.id.img_matziplist_visit_type)
-        val text_matziplist_visit_name = v.findViewById<TextView>(R.id.text_matziplist_visit_name)
-        val text_matziplist_visit_type = v.findViewById<TextView>(R.id.text_matziplist_visit_type)
-        val text_matziplist_visit_distance = v.findViewById<TextView>(R.id.text_matziplist_visit_distance)
-        val text_matziplist_visit_visitcount = v.findViewById<TextView>(R.id.text_matziplist_visit_visitcount)
-        val img_matziplist_visit_grade = v.findViewById<ImageView>(R.id.img_matziplist_visit_grade)
-        val matziplist_visit_wrapper = v.findViewById<LinearLayout>(R.id.matziplist_visit_wrapper)
-        val text_matziplist_visit_avgcost = v.findViewById<TextView>(R.id.text_matziplist_visit_avgcost)
+        val text_matziplist_seq = v.findViewById<TextView>(R.id.text_matziplist_seq)
+        val img_matziplist_type = v.findViewById<ImageView>(R.id.img_matziplist_type)
+        val text_matziplist_name = v.findViewById<TextView>(R.id.text_matziplist_name)
+        val text_matziplist_type = v.findViewById<TextView>(R.id.text_matziplist_type)
+        val img_matziplist_grade = v.findViewById<ImageView>(R.id.img_matziplist_grade)
+        val text_matziplist_value = v.findViewById<TextView>(R.id.text_matziplist_value)
 
         init {
-            matziplist_visit_wrapper.setOnClickListener {
+            v.setOnClickListener{
                 networkReceiver.setOnNetworkListener(this)
                 v.context.registerReceiver(networkReceiver, networkReceiver.getFilter())
             }
@@ -44,9 +37,9 @@ class MatZipListAdapter(private val itemList: ArrayList<ModelMatZipList>,
 
         override fun isConnected() {
             val intent = Intent(v.context, Detail::class.java)
-            intent.putExtra("visitcount", text_matziplist_visit_visitcount.text)
-            intent.putExtra("name", text_matziplist_visit_name.text)
-            intent.putExtra("type", text_matziplist_visit_type.text)
+            intent.putExtra("visitcount", itemList[adapterPosition].visitcount)
+            intent.putExtra("name", text_matziplist_name.text)
+            intent.putExtra("type", text_matziplist_type.text)
             intent.putExtra("locatex", itemList[adapterPosition].latlng?.x)
             intent.putExtra("locatey", itemList[adapterPosition].latlng?.y)
             intent.putExtra("area", area)
@@ -64,297 +57,72 @@ class MatZipListAdapter(private val itemList: ArrayList<ModelMatZipList>,
         }
     }
 
-    inner class DistanceViewHolder(private val v: View):RecyclerView.ViewHolder(v), NetworkConnectedListener{
-        private val networkReceiver = NetworkReceiver()
-
-        val matziplist_distance_wrapper = v.findViewById<LinearLayout>(R.id.matziplist_distance_wrapper)
-        val text_matziplist_distance_seq = v.findViewById<TextView>(R.id.text_matziplist_distance_seq)
-        val img_matziplist_distance_type = v.findViewById<ImageView>(R.id.img_matziplist_distance_type)
-        val text_matziplist_distance_name = v.findViewById<TextView>(R.id.text_matziplist_distance_name)
-        val text_matziplist_distance_type = v.findViewById<TextView>(R.id.text_matziplist_distance_type)
-        val text_matziplist_distance_distance = v.findViewById<TextView>(R.id.text_matziplist_distance_distance)
-        val text_matziplist_distance_visitcount = v.findViewById<TextView>(R.id.text_matziplist_distance_visitcount)
-        val img_matziplist_distance_grade = v.findViewById<ImageView>(R.id.img_matziplist_distance_grade)
-        val text_matziplist_distance_avgcost = v.findViewById<TextView>(R.id.text_matziplist_distance_avgcost)
-
-        init {
-            matziplist_distance_wrapper.setOnClickListener {
-                networkReceiver.setOnNetworkListener(this)
-                v.context.registerReceiver(networkReceiver, networkReceiver.getFilter())
-            }
-        }
-
-        override fun isConnected() {
-            val intent = Intent(v.context, Detail::class.java)
-            intent.putExtra("visitcount", text_matziplist_distance_visitcount.text)
-            intent.putExtra("name", text_matziplist_distance_name.text)
-            intent.putExtra("type", text_matziplist_distance_type.text)
-            intent.putExtra("locatex", itemList[adapterPosition].latlng?.x)
-            intent.putExtra("locatey", itemList[adapterPosition].latlng?.y)
-            intent.putExtra("area", area)
-            intent.putExtra("region", region)
-            intent.putExtra("avgCost", itemList[adapterPosition].avgcost)
-
-            v.context.startActivity(intent)
-
-            v.context.unregisterReceiver(networkReceiver)
-        }
-
-        override fun isNotConnected() {
-            Toast.makeText(v.context, "네트워크 연결을 확인해주세요.", Toast.LENGTH_SHORT).show()
-            v.context.unregisterReceiver(networkReceiver)
-        }
-
-    }
-
-    inner class AvgcostViewHolder(private val v:View):RecyclerView.ViewHolder(v), NetworkConnectedListener{
-        private val networkReceiver = NetworkReceiver()
-
-        val matziplist_avgcost_wrapper = v.findViewById<LinearLayout>(R.id.matziplist_avgcost_wrapper)
-        val text_matziplist_avgcost_seq = v.findViewById<TextView>(R.id.text_matziplist_avgcost_seq)
-        val img_matziplist_avgcost_type = v.findViewById<ImageView>(R.id.img_matziplist_avgcost_type)
-        val text_matziplist_avgcost_name = v.findViewById<TextView>(R.id.text_matziplist_avgcost_name)
-        val text_matziplist_avgcost_type = v.findViewById<TextView>(R.id.text_matziplist_avgcost_type)
-        val text_matziplist_avgcost_distance = v.findViewById<TextView>(R.id.text_matziplist_avgcost_distance)
-        val text_matziplist_avgcost_visitcount = v.findViewById<TextView>(R.id.text_matziplist_avgcost_visitcount)
-        val img_matziplist_avgcost_grade = v.findViewById<ImageView>(R.id.img_matziplist_avgcost_grade)
-        val text_matziplist_avgcost_avgcost = v.findViewById<TextView>(R.id.text_matziplist_avgcost_avgcost)
-
-        init {
-            matziplist_avgcost_wrapper.setOnClickListener {
-                networkReceiver.setOnNetworkListener(this)
-                v.context.registerReceiver(networkReceiver, networkReceiver.getFilter())
-            }
-        }
-
-        override fun isConnected() {
-            val intent = Intent(v.context, Detail::class.java)
-            intent.putExtra("visitcount", text_matziplist_avgcost_visitcount.text)
-            intent.putExtra("name", text_matziplist_avgcost_name.text)
-            intent.putExtra("type", text_matziplist_avgcost_type.text)
-            intent.putExtra("locatex", itemList[adapterPosition].latlng?.x)
-            intent.putExtra("locatey", itemList[adapterPosition].latlng?.y)
-            intent.putExtra("area", area)
-            intent.putExtra("region", region)
-            intent.putExtra("avgCost", itemList[adapterPosition].avgcost)
-
-            v.context.startActivity(intent)
-
-            v.context.unregisterReceiver(networkReceiver)
-        }
-
-        override fun isNotConnected() {
-            Toast.makeText(v.context, "네트워크 연결을 확인해주세요.", Toast.LENGTH_SHORT).show()
-            v.context.unregisterReceiver(networkReceiver)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when(viewType){
-            TYPE_VISIT->{
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_matziplist_visit_view, parent, false)
-                VisitViewHolder(view)
-            }
-            TYPE_DISTANCE->{
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_matziplist_distance_view, parent, false)
-                DistanceViewHolder(view)
-            }
-            TYPE_AVGCOST->{
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_matziplist_avgcost_view, parent, false)
-                AvgcostViewHolder(view)
-            }
-            else->{
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_matziplist_visit_view, parent, false)
-                VisitViewHolder(view)
-            }
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_matziplist_view, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return itemList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(holder){
-            is VisitViewHolder->{
-                holder.text_matziplist_visit_seq.text = itemList[position].seq
-                holder.text_matziplist_visit_avgcost.text = "평균 : "+itemList[position].avgcost.toString()+"원"
-                //타입 이미지
-                when(CheckType(itemList[position].type!!)){
-                    0->{
-                        //한식
-                    }
-                    1->{
-                        //일식
-                    }
-                    2->{
-                        //중식
-                    }
-                    3->{
-                        //양식
-                    }
-                    4->{
-                        //카페,디저트
-                    }
-                    5->{
-                        //분식
-                    }
-                    6->{
-                        //기타
-                    }
-                }
-                holder.text_matziplist_visit_name.text = itemList[position].name
-                //거리도 gps 값이 있을 때 없을 때 나눠야할 듯
-                if (itemList[position].distance != null){
-                    holder.text_matziplist_visit_distance.text = itemList[position].distance.toString() + "km"
-                }
-                else{
-                    holder.text_matziplist_visit_distance.text = holder.itemView.context.getString(R.string.no_distacne)
-                }
-
-                holder.text_matziplist_visit_type.text = itemList[position].type
-                holder.text_matziplist_visit_visitcount.text = itemList[position].visitcount.toString()
-                //등수는 seq 따라간다
-                when(itemList[position].seq.toInt()){
-                    1->{
-                        holder.img_matziplist_visit_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    2->{
-                        holder.img_matziplist_visit_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    3->{
-                        holder.img_matziplist_visit_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    else->{
-                        holder.img_matziplist_visit_grade.setImageDrawable(null)
-                    }
-                }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text_matziplist_seq.text = itemList[position].seq
+        when(CheckType(itemList[position].type!!)){
+            0->{
+                //한식
             }
-            is DistanceViewHolder->{
-                holder.text_matziplist_distance_seq.text = itemList[position].seq
-                holder.text_matziplist_distance_avgcost.text = "평균 : "+itemList[position].avgcost.toString()+"원"
-                //타입 이미지
-                when(CheckType(itemList[position].type!!)){
-                    0->{
-                        //한식
-                    }
-                    1->{
-                        //일식
-                    }
-                    2->{
-                        //중식
-                    }
-                    3->{
-                        //양식
-                    }
-                    4->{
-                        //카페,디저트
-                    }
-                    5->{
-                        //분식
-                    }
-                    6->{
-                        //기타
-                    }
-                }
-                holder.text_matziplist_distance_name.text = itemList[position].name
-                //거리도 gps 값이 있을 때 없을 때 나눠야할 듯
-                if (itemList[position].distance != null){
-                    holder.text_matziplist_distance_distance.text = itemList[position].distance.toString() + "km"
-                }
-                else{
-                    holder.text_matziplist_distance_distance.text = holder.itemView.context.getString(R.string.no_distacne)
-                }
-
-                holder.text_matziplist_distance_type.text = itemList[position].type
-                holder.text_matziplist_distance_visitcount.text = "방문 수: "+itemList[position].visitcount.toString()
-                //등수는 seq 따라간다
-                when(itemList[position].seq.toInt()){
-                    1->{
-                        holder.img_matziplist_distance_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    2->{
-                        holder.img_matziplist_distance_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    3->{
-                        holder.img_matziplist_distance_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    else->{
-                        holder.img_matziplist_distance_grade.setImageDrawable(null)
-                    }
-                }
+            1->{
+                //일식
             }
-            is AvgcostViewHolder->{
-                holder.text_matziplist_avgcost_seq.text = itemList[position].seq
-                holder.text_matziplist_avgcost_avgcost.text = ChangeCost(itemList[position].avgcost)
-                //타입 이미지
-                when(CheckType(itemList[position].type!!)){
-                    0->{
-                        //한식
-                    }
-                    1->{
-                        //일식
-                    }
-                    2->{
-                        //중식
-                    }
-                    3->{
-                        //양식
-                    }
-                    4->{
-                        //카페,디저트
-                    }
-                    5->{
-                        //분식
-                    }
-                    6->{
-                        //기타
-                    }
-                }
-                holder.text_matziplist_avgcost_name.text = itemList[position].name
-                //거리도 gps 값이 있을 때 없을 때 나눠야할 듯
-                if (itemList[position].distance != null){
-                    holder.text_matziplist_avgcost_distance.text = itemList[position].distance.toString() + "km"
-                }
-                else{
-                    holder.text_matziplist_avgcost_distance.text = holder.itemView.context.getString(R.string.no_distacne)
-                }
+            2->{
+                //중식
+            }
+            3->{
+                //양식
+            }
+            4->{
+                //카페,디저트
+            }
+            5->{
+                //분식
+            }
+            6->{
+                //기타
+            }
+        }
+        holder.text_matziplist_name.text = itemList[position].name
+        holder.text_matziplist_type.text = splitType(itemList[position].type)
 
-                holder.text_matziplist_avgcost_type.text = itemList[position].type
-                holder.text_matziplist_avgcost_visitcount.text = "방문 수: "+itemList[position].visitcount.toString()
-                //등수는 seq 따라간다
-                when(itemList[position].seq.toInt()){
-                    1->{
-                        holder.img_matziplist_avgcost_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    2->{
-                        holder.img_matziplist_avgcost_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    3->{
-                        holder.img_matziplist_avgcost_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
-                    }
-                    else->{
-                        holder.img_matziplist_avgcost_grade.setImageDrawable(null)
-                    }
-                }
+        when(itemList[position].seq.toInt()){
+            1->{
+                holder.img_matziplist_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
+            }
+            2->{
+                holder.img_matziplist_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
+            }
+            3->{
+                holder.img_matziplist_grade.setImageResource(R.drawable.ic_horizontal_rule_24px)
+            }
+            else->{
+                holder.img_matziplist_grade.setImageDrawable(null)
             }
         }
 
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return when(itemList[position].viewType){
-            TYPE_VISIT->{
-                TYPE_VISIT
+        when(itemList[position].viewType){
+            0->{
+                holder.text_matziplist_value.text = itemList[position].visitcount.toString()+"회"
             }
-            TYPE_DISTANCE->{
-                TYPE_DISTANCE
+            1->{
+                if (itemList[position].distance != null){
+                        holder.text_matziplist_value.text = itemList[position].distance.toString() + "km"
+                }
+                else{
+                    holder.text_matziplist_value.text = holder.itemView.context.getString(R.string.no_distacne)
+                }
             }
-            TYPE_AVGCOST->{
-                TYPE_AVGCOST
-            }
-            else->{
-                TYPE_VISIT
+            2->{
+                holder.text_matziplist_value.text = ChangeCost(itemList[position].avgcost)
             }
         }
     }
@@ -376,5 +144,11 @@ class MatZipListAdapter(private val itemList: ArrayList<ModelMatZipList>,
     private fun ChangeCost(cost: Int): String{
         val result: Double = cost / 10000.0
         return String.format("%.1f", result) + "만 원"
+    }
+
+    private fun splitType(type: String?): String?{
+        val splitTypes = type?.split(">")
+
+        return splitTypes?.get(1)
     }
 }
