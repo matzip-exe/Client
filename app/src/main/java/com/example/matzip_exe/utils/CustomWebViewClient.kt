c5bca7ff3778708dca8c38bcb3ab6f104b18aafd
Kim <rnlcksgrp1@naver.com>
2020-08-05 17:20:34 +0900

    MOD CustomWebViewClient remove qr tag

--- app/src/main/java/com/example/matzip_exe/utils/CustomWebViewClient.kt
+++ app/src/main/java/com/example/matzip_exe/utils/CustomWebViewClient.kt
@@ -2,12 +2,14 @@ package com.example.matzip_exe.utils
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class CustomWebViewClient: WebViewClient() {
 
     override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        Log.i("url", url)
         if (url != null) {
             if(url.startsWith("tel:")){
                 val intent = Intent(Intent.ACTION_DIAL, Uri.parse(url))
@@ -16,5 +18,11 @@ class CustomWebViewClient: WebViewClient() {
             }
         }
         return super.shouldOverrideUrlLoading(view, url)
+    }
+
+    override fun onLoadResource(view: WebView?, url: String?) {
+        super.onLoadResource(view, url)
+
+        view!!.loadUrl("javascript:if (typeof(document.getElementsByClassName('order_flow_wrap')[0]) != 'undefined' && document.getElementsByClassName('order_flow_wrap')[0] != null){document.getElementsByClassName('order_flow_wrap')[0].style.display = 'none';} void 0")
     }
 }
