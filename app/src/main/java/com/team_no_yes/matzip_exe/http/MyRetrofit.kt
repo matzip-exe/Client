ecdc995ebd5897a5fa2bbcb76453368405eb9e3a
CrewDaniel <alaskay@naver.com>
2022-04-24 18:15:59 +0900

    MOD server address

--- app/src/main/java/com/team_no_yes/matzip_exe/http/MyRetrofit.kt
+++ app/src/main/java/com/team_no_yes/matzip_exe/http/MyRetrofit.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MyRetrofit {
-    private val IP = "http://ec2-13-209-40-59.ap-northeast-2.compute.amazonaws.com:8080/user/"
+    private val IP = "http://ec2-54-180-30-37.ap-northeast-2.compute.amazonaws.com:8080/user/"
     private lateinit var retrofit: Retrofit
     private lateinit var interfaceRetrofit: InterfaceRetrofit
 
